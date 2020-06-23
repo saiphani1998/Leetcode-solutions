078fb4463aad059ce5bdf19ac210454bee3001ea
Sai Phani Krishna Pasumarthi <27241805+saiphani1998@users.noreply.github.com>
2020-06-23 22:39:52 +0530

    Added solution to count the nodes present in the given complete binary tree.

---
+++ June-LeetCoding-Challenge/CountCompleteTreeNodes.java
@@ -0,0 +1,60 @@
+/*
+* Given a complete binary tree, count the number of nodes.
+* Note: Definition of a complete binary tree from Wikipedia:
+* In a complete binary tree every level, except possibly the last, is completely filled,
+* and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+* Example:
+    Input:
+            1
+           / \
+          2   3
+         / \  /
+        4  5 6
+    Output: 6
+* */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        int leftDepth = leftDepth(root);
+        int rightDpeth = rightDepth(root);
+
+        if (leftDepth == rightDpeth) {
+            return (1 << leftDepth) - 1;
+        } else {
+            return countNodes(root.left) + countNodes(root.right) + 1;
+        }
+    }
+
+    private int leftDepth(TreeNode root) {
+        int height = 0;
+        while (root != null) {
+            height++;
+            root = root.left;
+        }
+        return height;
+    }
+
+    private int rightDepth(TreeNode root) {
+        int height = 0;
+        while (root != null) {
+            height++;
+            root = root.right;
+        }
+        return height;
+    }
+}
