53fd1f5ec65ebbd30b0f8696891b5ac2933dda86
Sai Phani Krishna Pasumarthi <27241805+saiphani1998@users.noreply.github.com>
2020-08-08 22:55:02 +0530

    Added solution to find the number of paths that sum to a given value.

--- August-Leetcoding-Challenge/PathSumIII.java
+++ August-Leetcoding-Challenge/PathSumIII.java
@@ -37,17 +37,20 @@
  */
 class Solution {
     public int pathSum(TreeNode root, int sum) {
-        if(root == null)
+        if(root == null) {
             return 0;
+        }
         return findPath(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
     }
 
     public int findPath(TreeNode root, int sum){
         int res = 0;
-        if(root == null)
+        if(root == null) {
             return res;
-        if(sum == root.val)
+        }
+        if(sum == root.val) {
             res++;
+        }
         res += findPath(root.left, sum - root.val);
         res += findPath(root.right, sum - root.val);
         return res;
