ebbe84d3843ca448377ae41689ddd852598c206b
Sai Phani Krishna Pasumarthi <27241805+saiphani1998@users.noreply.github.com>
2020-08-10 14:14:05 +0530

    Added solution to find the column number of given excel title.

---
+++ August-Leetcoding-Challenge/ExcelSheetColumNumber.java
@@ -0,0 +1,35 @@
+/*
+* Given a column title as appear in an Excel sheet, return its corresponding column number.
+* For example:
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+    ...
+* Example 1:
+    Input: "A"
+    Output: 1
+* Example 2:
+    Input: "AB"
+    Output: 28
+* Example 3:
+    Input: "ZY"
+    Output: 701
+* Constraints:
+    1 <= s.length <= 7
+    s consists only of uppercase English letters.
+    s is between "A" and "FXSHRXW".
+* */
+
+class Solution {
+    public int titleToNumber(String s) {
+        int column = 0;
+        for (int i = 0; i<s.length(); i++) {
+            column = (column*26) + (int)(s.charAt(i)-'A') + 1;
+        }
+        return column;
+    }
+}
