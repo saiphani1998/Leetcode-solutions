f0e929b0f080e2189b24e60d0e23dfb810d133ea
Sai Phani Krishna Pasumarthi <27241805+saiphani1998@users.noreply.github.com>
2020-08-21 22:28:27 +0530

    Added solution to segregate even and odd numbers in given array.

---
+++ August-Leetcoding-Challenge/SortArrayByParity.java
@@ -0,0 +1,27 @@
+/*
+* Given an array A of non-negative integers,
+* return an array consisting of all the even elements of A, followed by all the odd elements of A.
+* You may return any answer array that satisfies this condition.
+* Example 1:
+    Input: [3,1,2,4]
+    Output: [2,4,3,1]
+    The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+* Note:
+    1 <= A.length <= 5000
+    0 <= A[i] <= 5000
+* */
+
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int evenIndex = 0, val;
+        for (int currentIndex=0; currentIndex<A.length; currentIndex++) {
+            if ((A[currentIndex] & 1) != 1) {
+                val=A[evenIndex];
+                A[evenIndex]=A[currentIndex];
+                A[currentIndex]=val;
+                evenIndex++;
+            }
+        }
+        return A;
+    }
+}
