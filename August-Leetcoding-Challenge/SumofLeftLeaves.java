ef32b758d8561d616e48881dc7fe4fa6ee5b0458
Sai Phani Krishna Pasumarthi <27241805+saiphani1998@users.noreply.github.com>
2020-08-24 23:48:46 +0530

    Added solution to find the sum of left leaves of given binary tree.

---
+++ August-Leetcoding-Challenge/SumofLeftLeaves.java
@@ -0,0 +1,47 @@
+/*
+* Find the sum of all left leaves in a given binary tree.
+* Example:
+        3
+       / \
+      9  20
+        /  \
+       15   7
+    * There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+* */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int ans = 0;
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                ans += root.left.val;
+            } else {
+                ans += sumOfLeftLeaves(root.left);
+            }
+        }
+        if (root.right != null) {
+            ans += sumOfLeftLeaves(root.right);
+        }
+
+        return ans;
+    }
+}
